639e41a1f7f21754be2b4a60735c97d9b3754ce5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-12-18 13:18:27 +0700

    chore : remove report tracker seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,6 @@ class DatabaseSeeder extends Seeder
 
                 ReporterSeeder::class,
                 ReportSeeder::class,
-                ReportTrackerSeeder::class
             ]
         );
     }
