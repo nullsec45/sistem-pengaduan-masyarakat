91408fe6d89f552998f536346961ae1cdbec8532
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-10 23:38:49 +0700

    feat(media) : new model.

---
+++ app/Models/Media.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'model_type',
+        'model_id',
+        'uuid',
+        'collection_name',
+        'file_name',
+        'mime_type',
+        'disk',
+        'conversions_disk',
+        'size',
+        'manipulations',
+        'custom_properties',
+        'generated_conversions',
+        'responsive_images',
+        'order_column',
+    ];
+}
