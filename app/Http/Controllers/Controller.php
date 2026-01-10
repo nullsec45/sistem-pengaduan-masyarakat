3ebabc8def19ad91a04fc837632a061179dc859f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-10 23:37:59 +0700

    feat : construct helper in parent controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Helpers\Helpers;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected $helper;
+
+    public function __construct()
+    {
+        $this->helper = new Helpers();
+    }
 }
