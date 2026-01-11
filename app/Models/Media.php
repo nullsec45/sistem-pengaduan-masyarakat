1a7fc1ecb0bd38c633eeaa15e3dab8b4dea0a7a0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-11 23:54:49 +0700

    feat(media) : add accessor origina url.

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Media extends Model
 {
     use HasFactory;
+
+    protected $appends = ['original_url'];
+
 
     protected $fillable = [
         'model_type',
@@ -25,4 +29,10 @@ class Media extends Model
         'responsive_images',
         'order_column',
     ];
+
+    public function getOriginalUrlAttribute()
+    {
+        $url = $this->path . "/" . $this->file_name;
+        return Storage::url($url);
+    }
 }
