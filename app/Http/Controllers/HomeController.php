06976233496b60893c6686d56a98a4bee554f903
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-11 23:54:26 +0700

    feat : add variable home in index.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,13 +14,13 @@ class HomeController extends Controller
         $reports = Report::with(['reporter', 'category', 'tracker'])->paginate(10);
 
 
-        return Inertia::render('Home/Index', ['reports' => $reports]);
+        return Inertia::render('Home/Index', ['reports' => $reports, 'home' => true]);
     }
 
     public function tracker(string $id)
     {
         $report = Report::with('tracker')->findOrFail($id);
 
-        return Inertia::render('Home/StatusReportTracker', ['report' => $report, 'home' => true]);
+        return Inertia::render('Home/StatusReportTracker', ['report' => $report]);
     }
 }
